1e4366db17ee5a225545bbbd1f8386ad6a8a38a3
Gianluca Venturini <gianluca.91@gmail.com>
2014-09-28 18:24:00 -0500

    Test

--- src/test/graphic_manager.js
+++ src/test/graphic_manager.js
@@ -1,8 +1,13 @@
-function GraphicManager() {
+function GraphicManager(htmlId) {
 	this.lat = 41.8749077;
 	this.lon = -87.6368363;
 	this.scale = 10;
-	this.mapId = null;
+
+	this.mapId = htmlId;
+
+
+	this.map = null;
+	this.mapLayer = null;
 
 	this.svgs = [];
 	this.divs = [];
@@ -15,35 +20,46 @@ function GraphicManager() {
 	map type:
 
 */
-GraphicManager.prototype.createMap = function(htmlId, type) {
-	this.mapId = htmlId;
-	this.map = L.map(htmlId).setView([this.lat, this.lon], this.scale);
+GraphicManager.prototype.createMap = function(type) {
+	if(this.map == null)
+		this.map = L.map(this.mapId).setView([this.lat, this.lon], this.scale);
+
+	if(this.mapLayer != null)
+		this.map.removeLayer(this.mapLayer);
 
 	// Load the map
 	switch(type) {
 		case "normal":
 			this.mapLayer = L.tileLayer('http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
+					minZoom: 10,
+					maxZoom: 18,
 					attribution: ''
-				}).addTo(this.map);
+				});
+			this.mapLayer.addTo(this.map);
 			break;
 
 		case "satellitar":
 
 			this.mapLayer = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+				minZoom: 10,
+				maxZoom: 18,
 				attribution: ''
-			}).addTo(this.map);
+			});
+			this.mapLayer.addTo(this.map);
 			break;
 
 		default:
 
 			this.mapLayer = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
-				attribution: '',
-				maxZoom: 16
-			}).addTo(this.map);
+				minZoom: 10,
+				maxZoom: 18,
+				attribution: ''		
+			});
+			this.mapLayer.addTo(this.map);
 	}
 
-	this.mapWidth = +d3.select("#"+htmlId).style("width").slice(0, -2);
-	this.mapHeight = +d3.select("#"+htmlId).style("height").slice(0, -2);
+	this.mapWidth = +d3.select("#"+this.mapId).style("width").slice(0, -2);
+	this.mapHeight = +d3.select("#"+this.mapId).style("height").slice(0, -2);
 }
 
 /*
@@ -59,7 +75,8 @@ GraphicManager.prototype.addSvg = function(x, y, width, height) {
 	   .attr("_x", x)
 	   .attr("_y", y)
 	   .style("position", "absolute")
-	   .attr("viewBox","0 0 100 100");
+	   .attr("viewBox","0 0 100 100")
+	   .style("background-color", "rgba(200,200,200,0.5)");
 
 	this.svgs.push(svg);
 
@@ -76,12 +93,12 @@ GraphicManager.prototype.addSubMap = function(x, y, width, height, mapId, type) 
 	   .attr("id",mapId)
 	   .attr("class","submap");
 
-	var gm = new GraphicManager();
+	var gm = new GraphicManager(mapId);
 
 	this.divs.push(div);
 	this.positionDIVs();
 
-	gm.createMap(mapId, type);
+	gm.createMap(type);
 
 	
 	this.graphicManagers.push(gm);
@@ -120,7 +137,6 @@ GraphicManager.prototype.updateWindow = function() {
 	// Set the new dimension
 	this.mapWidth = +d3.select("#"+this.mapId).style("width").slice(0, -2);
 	this.mapHeight = +d3.select("#"+this.mapId).style("height").slice(0, -2);
-	console.log(this.mapWidth, this.mapHeight);
 
 	this.positionSVGs();
 	this.positionDIVs();
@@ -131,3 +147,20 @@ GraphicManager.prototype.updateWindow = function() {
 		gm.updateWindow();
 	}
 }
+
+/*
+	Test function that add a marker with a default icon
+*/
+GraphicManager.prototype.addMarker = function(lat, lon) {
+	var icon = L.icon({
+	    iconUrl: "http://cdn.leafletjs.com/leaflet-0.7.3/images/marker-icon.png",
+
+	    iconSize:     [23, 41], // size of the icon
+	    iconAnchor:   [10, 41], // point of the icon which will correspond to marker's location
+	});
+
+	var marker = L.marker([lat, lon], {
+		icon: icon
+	}).addTo(this.map);
+}
+
