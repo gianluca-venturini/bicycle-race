a340024cfb0900b2061ed92a35914123d9b7054a
Gianluca Venturini <gianluca.91@gmail.com>
2014-10-27 12:23:11 -0500

    Hour chart, x axis 2 -> 3 hour distance

--- src/library/graphic_manager.js
+++ src/library/graphic_manager.js
@@ -1638,7 +1638,7 @@ GraphicManager.prototype.updateGraphs = function () {
                 return (+a.hour) - (+b.hour);
             }), "hourOfDayMany", "fromStation", "Station");
             this.bikesHourDayComparison.setAxes("hour", "Hour", "count", "Bikes out");
-            this.bikesHourDayComparison.setTimeDataInX("hour", 2, "12hr");
+            this.bikesHourDayComparison.setTimeDataInX("hour", 3, "12hr");
             this.bikesHourDayComparison.setColor(["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"]);
             this.bikesHourDayComparison.draw();
 
